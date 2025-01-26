2b912008b2ddec5696863b53c6359776ab492e4c
Sajin PJ <sajinsjn4433@gmail.com>
2025-01-27 00:26:02 +0530

    basic navbar section created with react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './components/Navbar/Navbar';
-import Navbar from './components/Navbar/Navbar';
+import Navigationbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <Navigationbar />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,89 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-const Navbar = () => {
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Navigationbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg main-nav">
-      <div className="container">
-        {/* LOGO BAR */}
-        <a className="navbar-brand" href="/">
-          <img src="static/images/logo.png" alt="meetub" className="img-fluid logo-b" />
-        </a>
-
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="tf-ion-android-menu"></span>
-        </button>
-
-        {/* MAIN MENU */}
-        <div className="collapse navbar-collapse text-center" id="navbarNavDropdown">
-          <ul className="navbar-nav mx-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/login">Login</a>
-            </li>
-
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown1"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Pages
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown1">
-                <a className="dropdown-item" href="/about">About</a>
-                <a className="dropdown-item" href="/pricing">Tickets</a>
-                <a className="dropdown-item" href="/schedule">Schedule</a>
-                <a className="dropdown-item" href="/login">Login</a>
-                <a className="dropdown-item" href="/signup">Registration</a>
-                <a className="dropdown-item" href="/error">404 Page</a>
-                <a className="dropdown-item" href="/faq">Supports</a>
-              </div>
-            </li>
-
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown2"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Blog
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown2">
-                <a className="dropdown-item" href="/blog">Blog</a>
-                <a className="dropdown-item" href="/blog-single">Blog Single</a>
-              </div>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link" href="/contact">Contact</a>
-            </li>
-          </ul>
-
-          <a href="/pricing" className="btn btn-secondary btn-rounded mt-3 mt-lg-0">
-            Get Ticket
-          </a>
-        </div>
-      </div>
-    </nav>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Ezquare</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          {/* Centering the Nav items */}
+          <Nav
+            className="mx-auto my-2 my-lg-0"
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">About</Nav.Link>
+            <NavDropdown title="More" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Services</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#">
+              Connect
+            </Nav.Link>
+          </Nav>
+          {/* Placing button at the end of the navbar */}
+          <Button variant="outline-success">Get in touch</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default Navigationbar;
