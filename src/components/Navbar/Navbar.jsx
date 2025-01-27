414017eaae81c01af3e8675d8a79927f6b34266a
Sajin PJ <sajinsjn4433@gmail.com>
2025-01-27 23:04:54 +0530

    a basic Navbar has created with basic styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,29 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React, { useState } from 'react';
+import './Navbar.css';
 
 const Navigationbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Ezquare</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          {/* Centering the Nav items */}
-          <Nav
-            className="mx-auto my-2 my-lg-0"
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">About</Nav.Link>
-            <NavDropdown title="More" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Services</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#">
-              Connect
-            </Nav.Link>
-          </Nav>
-          {/* Placing button at the end of the navbar */}
-          <Button variant="outline-success">Get in touch</Button>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="navbar">
+      <div className="logo">MyLogo</div>
+      <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <button className="get-in-touch">Get in Touch</button>
+      <div
+        className={`menu-icon ${menuOpen ? 'rotated' : ''}`}
+        onClick={toggleMenu}
+      >
+        ☰
+      </div>
+    </nav>
   );
 };
 
